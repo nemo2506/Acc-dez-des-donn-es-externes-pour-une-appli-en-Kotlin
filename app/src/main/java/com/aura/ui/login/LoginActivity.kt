607a43b382909c4088ad4af8aff7e187548265ca
MARC NAVARRO <navamarc@gmail.com>
2025-04-02 01:31:29 +0200

    Bouton condition login visibility

--- app/src/main/java/com/aura/ui/login/LoginActivity.kt
+++ app/src/main/java/com/aura/ui/login/LoginActivity.kt
@@ -2,40 +2,51 @@ package com.aura.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
 import com.aura.databinding.ActivityLoginBinding
 import com.aura.ui.home.HomeActivity
 
 /**
  * The login activity for the app.
  */
-class LoginActivity : AppCompatActivity()
-{
+class LoginActivity : AppCompatActivity() {
 
-  /**
-   * The binding for the login layout.
-   */
-  private lateinit var binding: ActivityLoginBinding
+    /**
+     * The binding for the login layout.
+     */
+    private lateinit var binding: ActivityLoginBinding
 
-  override fun onCreate(savedInstanceState: Bundle?)
-  {
-    super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    binding = ActivityLoginBinding.inflate(layoutInflater)
-    setContentView(binding.root)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    val login = binding.login
-    val loading = binding.loading
+        val login = binding.login
+        val loading = binding.loading
 
-    login.setOnClickListener {
-      loading.visibility = View.VISIBLE
+        login.setOnClickListener {
+            loading.visibility = View.VISIBLE
 
-      val intent = Intent(this@LoginActivity, HomeActivity::class.java)
-      startActivity(intent)
+            val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+            startActivity(intent)
 
-      finish()
+            finish()
+        }
+
+        // Add listeners to update login button visibility
+        binding.identifier.doAfterTextChanged { isLoginReady() }
+        binding.password.doAfterTextChanged { isLoginReady() }
     }
-  }
 
+    private fun isLoginReady(): Unit {
+        val isIdentifierReady: Boolean = binding.identifier.text.toString().isNotEmpty()
+        val isPasswordReady: Boolean = binding.password.text.toString().isNotEmpty()
+        binding.login.visibility =
+            if (isIdentifierReady && isPasswordReady) View.VISIBLE else View.INVISIBLE
+    }
 }
