e656f2abdc93fbbc2cc616e06b48a013409e8e2f
MARC NAVARRO <navamarc@gmail.com>
2025-04-05 22:06:50 +0200

    Integration 2 message in strings.xml

--- app/src/main/java/com/aura/di/DataModule.kt
+++ app/src/main/java/com/aura/di/DataModule.kt
@@ -1,10 +1,12 @@
 package com.aura.di
 
+import android.content.Context
 import com.aura.data.network.ManageClient
 import com.aura.data.repository.BankRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -14,7 +16,10 @@ object DataModule {
 
     @Singleton
     @Provides
-    fun provideWeatherRepository(dataClient: ManageClient): BankRepository {
-        return BankRepository(dataClient)
+    fun provideBankRepository(
+        dataClient: ManageClient,
+        @ApplicationContext context: Context
+    ): BankRepository {
+        return BankRepository(dataClient, context)
     }
 }
