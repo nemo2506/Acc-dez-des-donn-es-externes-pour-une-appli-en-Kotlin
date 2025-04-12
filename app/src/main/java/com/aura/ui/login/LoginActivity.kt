73a5fe624d985af071c28f1fb0153235c693d7d1
MARC NAVARRO <navamarc@gmail.com>
2025-04-12 18:51:07 +0200

    Clean LoginActivity

--- app/src/main/java/com/aura/ui/login/LoginActivity.kt
+++ app/src/main/java/com/aura/ui/login/LoginActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
@@ -15,11 +14,7 @@ import kotlinx.coroutines.launch
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.core.view.isVisible
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.repeatOnLifecycle
 import com.aura.R
-import com.aura.domain.model.BalanceReportModel
-import com.aura.domain.model.LoginReportModel
 
 /**
  * The login activity for the app.
@@ -46,56 +41,28 @@ class LoginActivity : AppCompatActivity() {
             lifecycleScope.launch {
                 val id = binding.identifier.text.toString()
                 val password = binding.password.text.toString()
-                viewModel.getAuraLogin(id,password)
-                repeatOnLifecycle(Lifecycle.State.STARTED) {
-                    viewModel.uiState.collect {
-                        binding.loading.isVisible = it.isViewLoading
-                        if(it.logged == true){
-                            viewModel.getAuraBalance()
-                            homeLoader()
-                        }
+                viewModel.getAuraLogin(id, password)
+                viewModel.uiState.collect {
+                    binding.loading.isVisible = it.isViewLoading
+                    if (it.logged == true) {
+                        viewModel.getAuraBalance()
+                        homeLoader()
+                    }
 
-                        if (it.errorMessage?.isNotBlank() == true) {
-                            TODO()
-                            toastMessage(it.errorMessage)
-                        }
+                    if (it.errorMessage?.isNotBlank() == true) {
+                        toastMessage(it.errorMessage)
+                        binding.login.isEnabled = true
+                        binding.login.text = getString(R.string.try_again)
                     }
                 }
-//                manageIntentUI(isConnected)
             }
         }
     }
-
-
-//    private suspend fun manageIntentUI(connected: LoginReportModel) {
-//        if (connected.granted) {
-//
-//            if (getBalance()) {
-//                binding.loading.visibility = View.VISIBLE
-//                homeLoader()
-//            }
-//
-//        } else {
-//            binding.login.isEnabled = true
-//            toastMessage(connected.message)
-//        }
-//    }
 
     private fun homeLoader() {
         startActivity(Intent(this@LoginActivity, HomeActivity::class.java))
         finish()
     }
-
-//    private suspend fun getBalance(): Boolean {
-//        val report: BalanceReportModel = viewModel.getAuraBalance()
-//        if (report.balance == null) {
-//            report.message?.let { toastMessage(it) }
-//            binding.login.text = getString(R.string.try_again)
-//            binding.login.isEnabled = true
-//            return false
-//        }
-//        return true
-//    }
 
     private fun toastMessage(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
