496a436fbe6b4866533d98f1d53a0bfb7a4f31a5
MARC NAVARRO <navamarc@gmail.com>
2025-04-12 18:37:40 +0200

    Integration uiState on LoginActivity

--- app/src/main/java/com/aura/ui/login/LoginActivity.kt
+++ app/src/main/java/com/aura/ui/login/LoginActivity.kt
@@ -14,6 +14,9 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.repeatOnLifecycle
 import com.aura.R
 import com.aura.domain.model.BalanceReportModel
 import com.aura.domain.model.LoginReportModel
@@ -37,54 +40,62 @@ class LoginActivity : AppCompatActivity() {
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.login.setOnClickListener {
             binding.login.isEnabled = false
             lifecycleScope.launch {
-                viewModel.getAuraLogin(binding.identifier.text.toString(), binding.password.text.toString())
-//                val isConnected = isConnected(
-//                    binding.identifier.text.toString(),
-//                    binding.password.text.toString()
-//                )
+                val id = binding.identifier.text.toString()
+                val password = binding.password.text.toString()
+                viewModel.getAuraLogin(id,password)
+                repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    viewModel.uiState.collect {
+                        binding.loading.isVisible = it.isViewLoading
+                        if(it.logged == true){
+                            viewModel.getAuraBalance()
+                            homeLoader()
+                        }
+
+                        if (it.errorMessage?.isNotBlank() == true) {
+                            TODO()
+                            toastMessage(it.errorMessage)
+                        }
+                    }
+                }
 //                manageIntentUI(isConnected)
             }
         }
     }
 
 
-    private suspend fun manageIntentUI(connected: LoginReportModel) {
-        if (connected.granted) {
-
-            if (getBalance()) {
-                binding.loading.visibility = View.VISIBLE
-                homeLoader()
-            }
-
-        } else {
-            binding.login.isEnabled = true
-            toastMessage(connected.message)
-        }
-    }
+//    private suspend fun manageIntentUI(connected: LoginReportModel) {
+//        if (connected.granted) {
+//
+//            if (getBalance()) {
+//                binding.loading.visibility = View.VISIBLE
+//                homeLoader()
+//            }
+//
+//        } else {
+//            binding.login.isEnabled = true
+//            toastMessage(connected.message)
+//        }
+//    }
 
     private fun homeLoader() {
         startActivity(Intent(this@LoginActivity, HomeActivity::class.java))
         finish()
     }
 
-//    @RequiresApi(Build.VERSION_CODES.M)
-//    private suspend fun isConnected(id: String, password: String): LoginReportModel {
-//        return viewModel.getAuraLogin(id, password)
+//    private suspend fun getBalance(): Boolean {
+//        val report: BalanceReportModel = viewModel.getAuraBalance()
+//        if (report.balance == null) {
+//            report.message?.let { toastMessage(it) }
+//            binding.login.text = getString(R.string.try_again)
+//            binding.login.isEnabled = true
+//            return false
+//        }
+//        return true
 //    }
-
-    private suspend fun getBalance(): Boolean {
-        val report: BalanceReportModel = viewModel.getAuraBalance()
-        if (report.balance == null) {
-            report.message?.let { toastMessage(it) }
-            binding.login.text = getString(R.string.try_again)
-            binding.login.isEnabled = true
-            return false
-        }
-        return true
-    }
 
     private fun toastMessage(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
