0ce4fef81338992ede11fb3c03b3d87be01e6402
MARC NAVARRO <navamarc@gmail.com>
2025-04-28 00:20:13 +0200

    Update test LoginActivityViewModel

--- app/src/test/java/com/aura/ui/login/LoginActivityViewModelTest.kt
+++ app/src/test/java/com/aura/ui/login/LoginActivityViewModelTest.kt
@@ -1,5 +1,12 @@
 package com.aura.ui.login
 
+import androidx.lifecycle.SavedStateHandle
+import com.aura.data.repository.BankRepository
+import com.aura.ui.ConstantsApp
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.stackTracesAlignmentValueOf
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert.*
 
 import org.junit.Before
@@ -7,8 +14,12 @@ import org.junit.Test
 
 class LoginActivityViewModelTest {
 
+    private lateinit var dataRepository: BankRepository
+//    private val testDispatcher = StandardTestDispatcher()
+
     @Before
-    fun setUp() {
+    fun setup() {
+        dataRepository = mockk<BankRepository>()
     }
 
     @Test
@@ -16,7 +27,43 @@ class LoginActivityViewModelTest {
     }
 
     @Test
-    fun userDataControl() {
+    fun `test userDataControl with empty identifier and empty password`() = runTest {
+        val identifier = ""
+        val password = ""
+        val cut = LoginActivityViewModel(dataRepository)
+        cut.userDataControl(identifier.isNotEmpty(), password.isNotEmpty())
+        val uiState = cut.uiState.value
+        uiState.isUserDataReady?.let { assertFalse(it) }
+    }
+
+    @Test
+    fun `test userDataControl with existed identifier and empty password`() = runTest {
+        val identifier = "i"
+        val password = ""
+        val cut = LoginActivityViewModel(dataRepository)
+        cut.userDataControl(identifier.isNotEmpty(), password.isNotEmpty())
+        val uiState = cut.uiState.value
+        uiState.isUserDataReady?.let { assertFalse(it) }
+    }
+
+    @Test
+    fun `test userDataControl with empty identifier and existed password`() = runTest {
+        val identifier = ""
+        val password = "p"
+        val cut = LoginActivityViewModel(dataRepository)
+        cut.userDataControl(identifier.isNotEmpty(), password.isNotEmpty())
+        val uiState = cut.uiState.value
+        uiState.isUserDataReady?.let { assertFalse(it) }
+    }
+
+    @Test
+    fun `test userDataControl with existed identifier and existed password`() = runTest {
+        val identifier = "i"
+        val password = "p"
+        val cut = LoginActivityViewModel(dataRepository)
+        cut.userDataControl(identifier.isNotEmpty(), password.isNotEmpty())
+        val uiState = cut.uiState.value
+        uiState.isUserDataReady?.let { assertTrue(it) }
     }
 
     @Test
